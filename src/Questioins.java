951a0cda5c6d092b9059107db5f815d0b3be7cdf
jsing <j.singer200803@gmail.com>
2025-04-30 09:46:36 +0200

    making questions class

--- src/Questioins.java
+++ src/Questioins.java
@@ -1,5 +1,24 @@
 package src;
 
 public class Questioins {
+    private String question;
+    private String[] answers = new String[4];
+    private int correctIndex;
+    public Questioins(String question, String[] answers, int correctIndex){
+        this.question = question;
+        this.answers = answers;
+        this.correctIndex = correctIndex;
+    }
 
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public int getCorrectIndex() {
+        return correctIndex;
+    }
 }
